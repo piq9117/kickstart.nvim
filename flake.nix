e2bead238f493450014d0402a060b252e0267fdd
piq9117! <piq9117@gmail.com>
2024-01-31 15:33:45 -0500

    add flake file

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "nvim config";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+  outputs = {self, nixpkgs}:
+    let
+      supportedSystems = ["x86_64-linux"];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems(system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in {
+      overlay = final: prev: {};
+
+      packages = forAllSystems (system: {});
+
+      devShells = forAllSystems(system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gcc
+            ];
+            shellHook = "export PS1='[$PWD]\n❄ '";
+          };
+        });
+    };
+}
