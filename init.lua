458972a18f8683cb181444a1cb0ec2620bd9a6a8
piq9117 <piq9117@gmail.com>
2024-02-01 01:11:44 -0500

    separated orgmode config

--- init.lua
+++ init.lua
@@ -260,36 +260,6 @@ require('lazy').setup({
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
-  {
-    'nvim-orgmode/orgmode',
-    dependencies = {
-      {'nvim-treesitter/nvim-treesitter', lazy = true},
-    },
-    event = 'VeryLazy',
-    config = function()
-      require('orgmode').setup_ts_grammar()
-
-      require('orgmode').setup({
-        mappings = {
-          org = {
-            org_toggle_checkbox = '<leader><space>',
-          },
-        },
-      })
-
-      require('nvim-treesitter.configs').setup({
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { 'org' },
-        },
-        ensure_installed = {'org'}
-      })
-
-      require('orgmode').setup({
-        org_agenda_files = '~/code/notes/**/*'
-      })
-    end,
-  },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
